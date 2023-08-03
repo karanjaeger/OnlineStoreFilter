78cc7167bc5e0068e33bbc9dce7729afe0d977ae
karanjaeger <114157430+karanjaeger@users.noreply.github.com>
2023-08-04 04:38:22 +0530

    Modify resolution and aspect ratio

--- Assets/Scripts/ImageScaling.cs
+++ Assets/Scripts/ImageScaling.cs
@@ -1,15 +1,18 @@
+using System.Drawing;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ImageScaling : MonoBehaviour
 {
-    [SerializeField] private RectTransform mainBody;
+    [SerializeField] private RectTransform mainBody;    
     [SerializeField] private GameObject filterTab;
     private bool isOpen;
 
     private void Start()
     {
         filterTab.SetActive(false);
+        mainBody.sizeDelta = new Vector2(1920, mainBody.sizeDelta.y);
+        filterTab.GetComponent<RectTransform>().sizeDelta = new Vector2 (500, 1080);
         isOpen = false;
     }
 
@@ -17,16 +20,16 @@ public class ImageScaling : MonoBehaviour
     {
         if(!isOpen)
         {
-            ImageScale(1900, true);
+            ImageScale(1420, true);
         }
         else
         {
-            ImageScale(2400, false);
-        }
+            ImageScale(1920, false);
+        }   
     }
 
-    private void ImageScale (float size, bool control)
-    {
+    private void ImageScale (float size, bool control)    {
+        
         filterTab.SetActive(control);
         mainBody.sizeDelta = new Vector2(size, mainBody.sizeDelta.y);
         isOpen = control;
